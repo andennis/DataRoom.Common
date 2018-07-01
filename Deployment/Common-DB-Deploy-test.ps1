ea2eb152ecac0ed34f4817162b69519d1226f35e
Denis Andreev <dnandreev@hotmail.com>
2018-07-01 03:05:50 +0300

    #100 Fix path in Common-DB-Deploy-test.ps1

--- Deployment/Common-DB-Deploy-test.ps1
+++ Deployment/Common-DB-Deploy-test.ps1
@@ -6,4 +6,4 @@ if (-not (Test-Path $SqlPackageExe))
 	$SqlPackageExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\$SqlPackageVer\SqlPackage.exe"
 }
 
-& $SqlPackageExe /a:Publish /sf:"..\Tests\Common.Repository.Dapper.DB\bin\$config\Common.Repository.DB.dacpac" /pr:"..\Tests\Common.Repository.Dapper.DB\Common.Repository.DB.publish.xml"
+& $SqlPackageExe /a:Publish /sf:"..\Tests\Common.Repository.Dapper.DB\bin\$config\Common.Repository.DB.dacpac" /pr:"..\Tests\Common.Repository.DB\Common.Repository.DB.publish.xml"
